4972a25ca9d8786931de4f9ea90c7a86b660956e
Jesse Vaughn <jcv221@gmail.com>
2020-09-24 21:20:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
 low_value = nil
 name_hash.each do |key, value|
  # binding.pry
-  if low_value == nil or low_value < value
+  if low_value == nil or low_value > value
     low_value = value
     low_key = key
   end 
